c9ae8422326155bf279740211582bb14d7070b6e
STU <STU@DESKTOP-CIFSSDR>
2021-06-16 11:32:30 +0900

    ..

--- 09.객체간의관계[상속]/src/AcademyStaff.java
+++ 09.객체간의관계[상속]/src/AcademyStaff.java
@@ -1,27 +1,26 @@
 public class AcademyStaff extends AcademyMember {
-	public String depart;// 부서
-
+	public String depart;//부서
 	public AcademyStaff() {
-		// TODO Auto-generated constructor stub
 	}
-
-	public AcademyStaff(int no, String name, String depart) {
-		this.no = no;
-		this.name = name;
+	//alt+shift+s --> o
+	public AcademyStaff(int no,String name,String depart) {
+		this.no=no;
+		this.name=name;
 		this.depart = depart;
 	}
-
+	
 	public void staffPrint() {
 		this.print();
 		System.out.println(depart);
 	}
-
+	
+	//alt+shift+s --> r
 	public String getDepart() {
 		return depart;
 	}
-
 	public void setDepart(String depart) {
 		this.depart = depart;
 	}
-
+	
+	
 }
