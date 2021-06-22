9315a01a44f231d26f038c58200f46157a481105
STU <STU@DESKTOP-CIFSSDR>
2021-06-22 16:12:09 +0900

    0622

--- 18.예외[Exeption]/src/First.java
+++ 18.예외[Exeption]/src/First.java
@@ -1,6 +1,16 @@
+import java.io.IOException;
 
 public class First {
-	public void method1() {
-		
+	private Second second;
+	public First() {
+		second=new Second();
+	}
+	public void method1() throws IOException{
+		System.out.println(
+				"\t First.method1()실행");
+		second.method2();
+		System.out.println(
+				"\t First.method1()반환");
+		return;
 	}
 }
