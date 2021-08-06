93d933799239996ea1d099351ccbf85029c5d958
dongjkim <dongjkim@192.168.123.105>
2021-08-06 15:06:10 +0900

    ..

---
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/AddressService.java
@@ -0,0 +1,38 @@
+package dao.address.fourth;
+
+import java.util.ArrayList;
+
+/*
+AddressService객체
+ * - 주소록 비즈니스로직(업무)을 수행하는 클래스
+ * - Presentation객체(서블릿,JSP)에서 직접접근(메쏘드호출)하는 클래스
+ * - AddressDao객체를 멤버변수로 가지고있다.
+   - 주소록 업무수행메쏘드에서 DataBase접근(CRUD)이 필요하면 Dao를 사용한다.
+ */
+public class AddressService {
+	private AddressDao addressDao;
+	
+	public AddressService() {
+		addressDao=new AddressDao();
+	}
+	
+	public Address selectByNo(int no) throws Exception {
+		Address address = addressDao.selectByNo(no);
+		return address;
+	}
+	public ArrayList<Address> selectAll() throws Exception {
+		ArrayList<Address> addressList = addressDao.selectAll();
+		return addressList;
+	}
+	public int insert(Address address) throws Exception {
+		int insertRowCount = addressDao.insert(address);
+		return insertRowCount;
+	}
+	public int deleteByNo(int no) throws Exception {
+		return addressDao.deleteByNo(no);
+		
+	}
+	public int updateByNo(Address updateAdress) throws Exception {
+		return addressDao.updateByNo(updateAdress);
+	}
+}
---
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/AddressServiceTestMain.java
@@ -0,0 +1,14 @@
+package dao.address.fourth;
+
+public class AddressServiceTestMain {
+
+	public static void main(String[] args) throws Exception{
+		AddressService addressService = new AddressService();
+		System.out.print("1. insert --> "+addressService.insert(new Address(0,"ooo","오오오","555-5555","오정리")));
+		System.out.println("2.update    -->" + addressService.updateByNo(new Address(1, "first", "일일일", "111-1111", "일산")));
+		System.out.println("3.selectByNo-->" + addressService.selectByNo(6));
+		System.out.println("4.deleteByNo-->" + addressService.deleteByNo(14));
+		System.out.println("5.selectAll-->" + addressService.selectAll());
+	}
+
+}
